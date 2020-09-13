1402bd005eff18d916756ea95041ef5039682d80
Beardy <abolhovitins@gmail.com>
2020-09-13 13:42:28 +0100

    added FlatList with dummy data

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,27 +1,39 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { Button, StyleSheet, Text, View, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGirdItem = (itemData) => {
+  return (
+    <View style={styles. gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 function CategoriesScreen(props) {
-
-    return (
-<View style={styles.screen}>
-    <Text>The Categories screen!</Text>
-    <Button title='Go to meals!' onPress={() => {
-        props.navigation.navigate({
-            routeName: 'CategoryMeals'
-        })
-        // props.navigation.navigate('CategoryMeals') <--- also works
-    }} />
-</View>
-    )
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGirdItem}
+      numColumns={2}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gridItem:{
+      flex:1,
+      margin: 15,
+      height: 150,
+  }
+});
 
-export default CategoriesScreen
+
+
+export default CategoriesScreen;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 function CategoryMealsScreen(props) {
-    return (
-<View style={styles.screen}>
-    <Text>The Category Meals screen!</Text>
-    <Button title='Go to meal detail!' onPress={() => {
-        props.navigation.navigate({
-            routeName: 'MealDetail'
-        })    }} />
-</View>
-    )
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meals screen!</Text>
+      <Button
+        title="Go to meal detail!"
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+          });
+        }}
+      />
+
+      <Button title='Go Back' onPress={() =>{
+          props.navigation.pop();
+      }}/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default CategoryMealsScreen
+export default CategoryMealsScreen;
